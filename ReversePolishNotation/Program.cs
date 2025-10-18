af87b237649b38e2f6419ade6716376fdf1d2d07
Линар <159605589+d1ssmuss@users.noreply.github.com>
2025-10-19 01:26:34 +0400

    Update Program.cs

--- ReversePolishNotation/Program.cs
+++ ReversePolishNotation/Program.cs
@@ -1,36 +1,158 @@
-﻿using System.Text;
+using System.Text;
 
 string str = "";
-
+string result = "";
+Console.OutputEncoding = Encoding.UTF8; // для вывода в консоль символ π
 // Тесты
 // Сделать пару тестов deepseek
 // Убрать пробелы везде?
 string str_test1 = "3+2*(6-4)";
-string str_test2 = "4+3*(6-√(2))-ln(10/sin(π/6))+(3000-13%)";
+string str_test2 = "4+3*(6-√(2))-ln(10/sin(" + "\u03c0" + "/6))+(3000-13%)";
+Console.WriteLine(str_test2);
+Console.WriteLine();
 
-void TypeAllSymbols(string stroka)
+str = str_test2;
+char pi = 'π';
+string trigonometric = "";
+
+// Console.WriteLine(char.IsLetter(pi)); // π - это буква
+
+
+// Создаю стек
+Stack<string> stack = new Stack<string>(); // операций и скобки
+
+
+// Я хочу создать словарь приоритетов операций
+var priority_operation = new Dictionary<string, int>
 {
-    foreach(char c in stroka)
+    { "+", 1 },
+    { "-", 1 },
+    { "*", 2 },
+    { "/", 2 },
+    { "^", 3 }
+};
+
+// Console.WriteLine(priority_operation["^"]);
+// Console.WriteLine(char.IsLetter('√')); // False
+
+
+for (int i = 0; i < str.Length; i++)
+{
+    char current_element = str[i]; // Текущий элемент
+    if (char.IsDigit(current_element) || current_element == pi || current_element == 'e' || current_element == '%')
     {
-        if (char.IsDigit(c))
+        Console.WriteLine(i.ToString() + ". " + "Добавляю " + current_element + " в результат");
+        result += current_element;
+    }
+    else if (char.IsLetter(current_element)) // текущий символ - буква, используется для sin cos tg ctg abs lg ln 
+    {
+        trigonometric += current_element;
+    }
+    else if (current_element.ToString() != ")")
+    {
+        if (trigonometric.Length > 0) // Нашли выражение
         {
-            Console.WriteLine(c + " - это цифра");
+            stack.Push(trigonometric);
+            Console.WriteLine(i.ToString() + ". " + "Добавляю " + trigonometric + " в стек");
+            trigonometric = ""; // обнуляем строку тригонометрик(тригонометрического выражения)
         }
         else
         {
-            Console.WriteLine(c + " - это символ");
+            // Возможно здесь будет ошибка в самом начале, про что я имею ввиду
+            // У нас в начале стек пустой, и как он будет сравнивать какой приоритет у знака в стеке(которого нет) ?
+            // М.б опять проверять на стке больше нуля, иначе просто добавлять (push)
+            // Лол, это действительно так!
+            if (stack.Count > 0 && (stack.Peek().ToString() == "+" || stack.Peek().ToString() == "-" || stack.Peek().ToString() == "*" || stack.Peek().ToString() == "/"))
+            {
+                while ((stack.Count > 0) && (priority_operation[stack.Peek()] > priority_operation[current_element.ToString()])) // && stack.Count > 0
+                {
+                    result += stack.Pop();
+                }
+            }
+            stack.Push(current_element.ToString());
+            Console.WriteLine(i.ToString() + ". " + "Добавляю " + current_element + " в стек");
+        }
+
+
+        // Console.WriteLine(i.ToString() + ". " + "Добавляю " + current_element + " в стек");
+    }
+    else if (current_element.ToString() == ")")
+    {
+        while (stack.Count > 0 && stack.Peek() != "(")
+        {
+            var t = stack.Peek();
+            result += stack.Pop();
+            Console.WriteLine(i.ToString() + ". " + "Убираю из стека " + t + " и добавляю " + t + " в результат");
+        }
+        if (stack.Count > 0 && stack.Peek() == "(") // А можно ли убрать здесь stack.Count > 0 ?
+        {
+            var p = stack.Pop(); // Убираем "("
+            Console.WriteLine(i.ToString() + ". " + "Убираю из стека " + p);
         }
     }
 }
 
-TypeAllSymbols(str_test1);
-TypeAllSymbols(str_test2);
-Console.OutputEncoding = Encoding.UTF8; // для вывода в консоль символ π
-if ("\u03c0" == 'π'.ToString())
+
+
+
+Console.WriteLine("Результат: " + result);
+
+if (result == "4362√-*10π6/sin/ln-300013%-++")
 {
-    Console.WriteLine(1);
+    for (int i = 0; i < 13; i++)
+    {
+        Console.WriteLine("ПОБЕДА");
+    }
 }
-else
-{
-    Console.WriteLine(2);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void TypeAllSymbols(string stroka)
+//{
+//    foreach(char c in stroka)
+//    {
+//        if (char.IsDigit(c))
+//        {
+//            Console.WriteLine(c + " - это цифра");
+//        }
+//        else
+//        {
+//            Console.WriteLine(c + " - это символ");
+//        }
+//    }
+//}
+
+//TypeAllSymbols(str_test1);
+//TypeAllSymbols(str_test2);
+//if ("\u03c0" == 'π'.ToString())
+//{
+//    Console.WriteLine(1);
+//}
+//else
+//{
+//    Console.WriteLine(2);
+//}
+
